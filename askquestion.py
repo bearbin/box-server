1de101fe646b8370ca89e424c3bfea8fe854ee74
Alexander Harkness <bearbin@gmail.com>
2013-05-11 21:24:46 +0100

    Updated askquestion to python 3 and also did a bit more. Interactive demos and tests coming soon.

--- askquestion.py
+++ askquestion.py
@@ -1,45 +1,48 @@
-def ask_question(questionText, answerTypeAllowed = 0, allowedAnswers = None, acceptedAnswerText = "Answer accepted.", typeErrorText = "Your answer was of the wrong type.", badAnswerText = "Your answer was not one of the allowed answers.", genericErrorText = "An error occured."):
+def ask(questionText, answerTypeAllowed = 0, allowedAnswers = None, acceptedAnswerText = "Answer accepted.", typeErrorText = "Your answer was of the wrong type.", badAnswerText = "Your answer was not one of the allowed answers.", genericErrorText = "An error occured."):
+    """
+    Ask a question on the command line.
     
-    # answerTypeAllowed : What type of answers to allow. Defaults to any type.
-    # questionText      : What question the user is asked. This is required.
-    # allowedAnswers    : What values are allowed. Defaults to any.
-    # typeErrorText     : The text printed if the input was of the wrong type.
-    # badAnswerText     : What is printed when the answer is not on eof the specified allowed ones.
-    # genericErrorText  : What happens when an error occurs.
-    #
-    # answerTypeAllowed Values:
-    # 0 : Any type.
-    # 1 : String
-    # 2 : Integer
-    # 3 : Float
-    # 4 : Boolean 
+    answerTypeAllowed : What type of answers to allow. Defaults to any type.
+    questionText      : What question the user is asked. This is required.
+    allowedAnswers    : What values are allowed. Defaults to any.
+    typeErrorText     : The text printed if the input was of the wrong type.
+    badAnswerText     : What is printed when the answer is not on eof the specified allowed ones.
+    genericErrorText  : What happens when an error occurs.
+    
+    answerTypeAllowed Values:
+    0 : Any type.
+    1 : String
+    2 : Integer
+    3 : Float
+    4 : Boolean 
+    """
 
     # TODO: Simplify more.
 
     def badAnswer():
-        print badAnswerText
-        raw_input("Press enter to continue. ")
+        print(badAnswerText)
+        input("Press enter to continue. ")
 
-    if answerTypeAllowed not in range(5):
-        print "There was an error with internal coding :("
-        print "Please tell the maintainer this error:"
-        print "answerTypeAllowed out of bounds when ask_question function called."
-        print "answerTypeAllowed was "+str(answerTypeAllowed)+" when allowed values are 0-4."
+    if answerTypeAllowed not in list(range(5)):
+        print("There was an error with internal coding :(")
+        print("Please tell the maintainer this error:")
+        print("answerTypeAllowed out of bounds when ask_question function called.")
+        print("answerTypeAllowed was "+str(answerTypeAllowed)+" when allowed values are 0-4.")
         return
 
     while 1:
-        userInput = raw_input(questionText)
+        userInput = input(questionText)
         if allowedAnswers is not None:		
             if answerTypeAllowed == 0:
                 if userInput in allowedAnswers:
-                    print acceptedAnswerText
+                    print(acceptedAnswerText)
                     return userInput
                 else:
                     badAnswer()
                     continue
             elif answerTypeAllowed == 1:
                 if str(userInput) in allowedAnswers:
-                    print acceptedAnswerText
+                    print(acceptedAnswerText)
                     return str(userInput)
                 else:
                     badAnswer()
@@ -48,11 +51,11 @@ def ask_question(questionText, answerTypeAllowed = 0, allowedAnswers = None, acc
                 try:
                     int(userInput)
                 except ValueError:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
                 if int(userInput) in allowedAnswers:
-                    print acceptedAnswerText
+                    print(acceptedAnswerText)
                     return int(userInput)
                 else:
                     badAnswer()
@@ -61,11 +64,11 @@ def ask_question(questionText, answerTypeAllowed = 0, allowedAnswers = None, acc
                 try:
                     float(userInput)
                 except ValueError:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
                 if float(userInput) in allowedAnswers:
-                    print acceptedAnswerText
+                    print(acceptedAnswerText)
                     return float(userInput)
                 else:
                     badAnswer()
@@ -73,61 +76,64 @@ def ask_question(questionText, answerTypeAllowed = 0, allowedAnswers = None, acc
             elif answerTypeAllowed == 4:
                 if str(userInput).lower() in ['y', 'n', 'yes', 'no', '0', '1', 'true', 'false']:
                     if str(userInput).lower() in ['y', 'yes', '1', 'true']:
-                        print acceptedAnswerText
+                        print(acceptedAnswerText)
                         return True
                     else:
-                        print acceptedAnswerText
+                        print(acceptedAnswerText)
                         return False
                 else:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
             else:
-                print genericErrorText
-                raw_input("Press enter to continue. ")
+                print(genericErrorText)
+                input("Press enter to continue. ")
                 continue
         elif allowedAnswers is None:
             if answerTypeAllowed == 0:
-                print acceptedAnswerText
+                print(acceptedAnswerText)
                 return userInput
             elif answerTypeAllowed == 1:
-                print acceptedAnswerText
+                print(acceptedAnswerText)
                 return str(userInput)
             elif answerTypeAllowed == 2:
                 try:
                     int(userInput)
                 except ValueError:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
-                print acceptedAnswerText
+                print(acceptedAnswerText)
                 return int(userInput)
             elif answerTypeAllowed == 3:
                 try:
                     float(userInput)
                 except ValueError:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
-                print acceptedAnswerText
+                print(acceptedAnswerText)
                 return float(userInput)
             elif answerTypeAllowed == 4:
                 if str(userInput).lower() in ['y', 'n', 'yes', 'no', '0', '1', 'true', 'false']:
                     if str(userInput).lower() in ['y', 'yes', '1', 'true']:
-                        print acceptedAnswerText
+                        print(acceptedAnswerText)
                         return True
                     else:
-                        print acceptedAnswerText
+                        print(acceptedAnswerText)
                         return False
                 else:
-                    print typeErrorText
-                    raw_input("Press enter to continue. ")
+                    print(typeErrorText)
+                    input("Press enter to continue. ")
                     continue
             else:
-                print genericErrorText
-                raw_input("Press enter to continue. ")
+                print(genericErrorText)
+                input("Press enter to continue. ")
                 continue
         else:
-            print genericErrorText
-            raw_input("Press enter to continue. ")
+            print(genericErrorText)
+            input("Press enter to continue. ")
             continue
+
+if __name__ == "__main__":
+    print("Testing testing 123")
